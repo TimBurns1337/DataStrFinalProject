321682e12f2a4ba1a942cf97a97960bdcd5f89f1
Tim Burns <tim.marshall.burns95@gmail.com>
2021-04-09 14:12:41 -0400

    Add project files.

---
+++ DataStrFinalProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStrFinalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
