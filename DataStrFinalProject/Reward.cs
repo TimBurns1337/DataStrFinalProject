7315ae095d8504ee8a72cecccc1eb0dc307e7625
Tim Burns <tim.marshall.burns95@gmail.com>
2021-04-26 18:05:19 -0400

    working on rewards validation

--- DataStrFinalProject/Reward.cs
+++ DataStrFinalProject/Reward.cs
@@ -18,48 +18,53 @@ namespace DataStrFinalProject
             WeaponType = myHero.WeaponType;
 
             /*int rewardType = rd.Next(1, 5);*/ //all kind of reward, not random
-            switch (reward)
-            {
-                case 1:
-                    Console.WriteLine("You loot a fine boots," +
-                        " but it does not fit your size(+25 Spd -10 HP). Do you want to equip it? yes or no");
-                    string ans = Console.ReadLine();
-                    if (ans == "yes")
-                    {
-                        Speed += 25;
-                        Health -= 10;
-                    }
-                    else
-                    {
-                        Console.WriteLine("you silently plan to sell the boots in next town if there is one.");
-                    }                    
-                    break;
-                case 2:
-                    Strength += 25;
-                    Console.WriteLine("You find a precious ore from the loot. Your sword is sharpened(+25 Strength)");
-                    weaponLevel++;
-                    break;
-                case 3:
-                    Console.WriteLine("You find some pills from the enemy body, are you gonna eat it? yes or no");
-                    string ans1 = Console.ReadLine();
-                    int pill = rd.Next(1, 3);
-                    if (ans1.Equals("yes") && pill % 2 == 0 )
-                    {
-                        Console.WriteLine("Its chocolote, and your pain is alleviated(HP +50)");
-                        Health += 50;
-                    }
-                    else if(ans1.Equals("yes") && pill % 2 == 1)
-                    {
-                        Console.WriteLine("It's fish bait, ew~");
-                    }
-                    break;
-                case 4:
-                    Console.WriteLine("It's an honorable fight! you bury the opponent's body and restart you jounery. ");
-                    break;
+            //while (true)
+            //{
 
-                default:
-                    break;
-            }
+
+                switch (reward)
+                {
+                    case 1:
+                        Console.WriteLine("You loot a fine boots," +
+                            " but it does not fit your size(+25 Spd -10 HP). Do you want to equip it? yes or no");
+                        string ans = Console.ReadLine();
+                        if (ans == "yes")
+                        {
+                            Speed += 25;
+                            Health -= 10;
+                        }
+                        else
+                        {
+                            Console.WriteLine("you silently plan to sell the boots in next town if there is one.");
+                        }
+                        break;
+                    case 2:
+                        Strength += 25;
+                        Console.WriteLine("You find a precious ore from the loot. Your sword is sharpened(+25 Strength)");
+                        weaponLevel++;
+                        break;
+                    case 3:
+                        Console.WriteLine("You find some pills from the enemy body, are you gonna eat it? yes or no");
+                        string ans1 = Console.ReadLine();
+                        int pill = rd.Next(1, 3);
+                        if (ans1.Equals("yes") && pill % 2 == 0)
+                        {
+                            Console.WriteLine("Its chocolote, and your pain is alleviated(HP +50)");
+                            Health += 50;
+                        }
+                        else if (ans1.Equals("yes") && pill % 2 == 1)
+                        {
+                            Console.WriteLine("It's fish bait, ew~");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("It's an honorable fight! you bury the opponent's body and restart you jounery. ");
+                        break;
+
+                    default:
+                        break;
+                }
+            //}
 
         }
 
